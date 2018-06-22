7bdc578de6fa29716f5834079667e3faa54ae03f
Mounica Kamireddy <kamireddym28@gmail.com>
2018-06-21 23:11:47 -0700

    Bamking app DB

---
+++ database.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy import Enum
+import datetime
+from sqlalchemy import DateTime
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    email = Column(String(250), primary_key=True)
+    password = Column(String(250), nullable=False)
+
+class Account(Base):
+    __tablename__ = "account"
+
+    account_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.email'))
+    balance = Column(Integer)
+    user = relationship(User)
+
+class Transaction(Base):
+    __tablename__ = 'transaction'
+
+    transaction_id = Column(Integer, primary_key=True)
+    account_id = Column(Integer, ForeignKey('account.account_id'))
+    amount = Column(Integer)
+    transaction_type = Column(Enum('deposit','Withdrawl'))
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    account = relationship(Account)
+
+engine = create_engine('sqlite:///bankAccount.db')
+
+Base.metadata.create_all(engine)
